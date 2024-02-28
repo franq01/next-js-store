ff2de8f135765a83ed69967738adb2f221e320e5
franq01 <carreone705@gmail.com>
2024-02-28 12:46:12 -0600

    layout

---
+++ src/app/store/[category]/layout.tsx
@@ -0,0 +1,8 @@
+export default function Layout({children}:{children:React.ReactNode}){
+    return(
+        <main>
+            <nav>navegagcoon de las categorias</nav>
+            {children}
+        </main>
+    )
+}
