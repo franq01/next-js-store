44d1e9bd89a0127d5c136f2a8ee7c81f2307acb4
franq01 <carreone705@gmail.com>
2024-02-28 13:27:05 -0600

    manejo para metros de rutas

--- src/app/store/[[...category]]/page.tsx
+++ src/app/store/[[...category]]/page.tsx
@@ -1,11 +1,12 @@
 interface CategoryProps{
     params:{
-        category: string
+        category: string[]
     }
 }
 
 export default function category (props:CategoryProps)
 { const {category} = props.params
+console.log(category)
     return(
         <h1>category dinamic {category}</h1>
     )
--- src/app/store/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function store(){
-return(
-    <h1>pague store </h1>
-)
-}
